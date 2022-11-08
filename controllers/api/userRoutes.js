d1033c47b0f65a850ee10c521cf86da39c9f1fd1
NG <nicholeguan2@gmail.com>
2022-11-08 01:38:43 -0800

    rename name in User model to username and create post route to create a new user when sign-up button is clicked

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+router.post('/', async (req, res) => {
+    try {
+        const userData = await User.create(req.body);
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.json(userData);
+        });
+
+    } catch (err) {
+        res.json(err);
+    }
+});
+
+module.exports = router;
