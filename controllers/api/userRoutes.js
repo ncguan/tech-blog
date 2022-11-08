c327acde721596bec08b960220ac52621c26bded
NG <nicholeguan2@gmail.com>
2022-11-08 02:23:11 -0800

    create post route to login user that is in database

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -17,6 +17,34 @@ router.post('/', async (req, res) => {
     }
 });
 
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({ where: { username: req.body.username } });
+
+        if (!userData) {
+            res.json({ message: 'Incorrect username or password. Please try again' });
+            return;
+        }
+
+        const validPassword = await userData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.json({ message: 'Incorrect email or password. Please try again' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.json(userData);
+        });
+
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
 router.post('/logout', (req, res) => {
     if (req.session.logged_in) {
         req.session.destroy(() => { res.end(); });
--- models/User.js
+++ models/User.js
@@ -2,7 +2,12 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
-class User extends Model {}
+class User extends Model {
+  checkPassword(loginPassword) {
+    return bcrypt.compareSync(loginPassword, this.password);
+  }
+
+}
 
 User.init(
   {
